a69b6056460542d564ee9b2877408be7ec863af0
Edward Hoang <edward.hoang@sjsu.edu>
2020-12-28 01:15:31 -0800

    Change baseURL for deployment

--- src/apis/streams.js
+++ src/apis/streams.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+// export default axios.create({
+//     baseURL: "http://localhost:3001"
+// });
+
 export default axios.create({
-    baseURL: "http://localhost:3001"
+    baseURL: "https://streamy-db-edward.herokuapp.com"
 });
